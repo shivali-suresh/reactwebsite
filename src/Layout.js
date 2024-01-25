3529b78f51da4751d182881ae3177d3fe2128ebe
shivali <shivalikec@gmail.com>
2024-01-25 17:57:11 +0530

    first commit

---
+++ src/Layout.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import "./Layout.css";
+import {Outlet,Link}from "react-router-dom";
+export default function Layout() {
+  return (
+    <div>
+        <nav>
+            <ul className="nav-bar">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+                <li><Link to="/service">Service</Link></li>
+
+            </ul>
+        </nav>
+        <Outlet/>
+    </div>
+  )
+}
+
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Service from './Service'
+
+export default function Home() {
+  return (
+    <div>Home</div>
+  )
+}
